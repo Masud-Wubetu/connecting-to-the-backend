ec29a1e2492695222a5a6a5e14d295271ce7e83a
masud wubetu <wubetumasud@gmail.com>
2025-11-21 22:49:43 +0300

    Deleting Data

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,25 @@ function App() {
     return () => controller.abort();
   }, [])
 
+  const deleteUser = (user: User) => {
+     const originalUsers = [...users];
+     setUsers(users.filter(u => u.id !== user.id));
+
+
+     axios.delete('https://jsonplaceholder.typicode.com/users/' + user.id)
+       .catch(err => {
+         setError(err.message);
+         setUsers(originalUsers);
+       });
+  }
+
   return (
     <>
       { isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      <ul className="list-group">
+        {users.map(user => <li key={user.id} className="list-group-item d-flex justify-content-between">{user.name}
+          <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button></li>)}
       </ul>
    </>
   );
