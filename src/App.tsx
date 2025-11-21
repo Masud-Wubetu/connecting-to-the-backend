a5af3320f0fcb3044a268a4e846546128a3458d6
masud wubetu <wubetumasud@gmail.com>
2025-11-21 22:19:55 +0300

    showing a Loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,22 @@ interface User{
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     axios
      .get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
-     .then(res => setUsers(res.data))
+     .then(res => {
+      setUsers(res.data);
+      setLoading(false);
+     })
      .catch(err => {
         if(err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -28,6 +34,7 @@ function App() {
 
   return (
     <>
+      { isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
